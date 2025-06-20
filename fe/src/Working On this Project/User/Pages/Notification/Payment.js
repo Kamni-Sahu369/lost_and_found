7752ee7cff6ef5eea8ed2bb31d49d0dd3fc8d349
Ankitkumar-dewangan <ankitdewangan1122@gmail.com>
2025-06-20 17:29:37 +0530

    ankit

--- fe/src/Working On this Project/User/Pages/Notification/Payment.js
+++ fe/src/Working On this Project/User/Pages/Notification/Payment.js
@@ -1,31 +1,44 @@
 // src/components/CheckoutButton.js
-import React from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePromise = loadStripe('pk_test_51Rbg3MQTfWQgOkzpBqJZktgOL1U9TVvyZ3qoqxrygWmK7BQ1HgNUJ0eBc7NFFme7D8b7I5HFf4UC9ETmMLjsQUWN00hYuyPu7N'); // Replace with your key
+import React, { useState , useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { get_userPayments } from "../../../Api/Service";
+const stripePromise = loadStripe(
+  "pk_test_51Rbg3MQTfWQgOkzpBqJZktgOL1U9TVvyZ3qoqxrygWmK7BQ1HgNUJ0eBc7NFFme7D8b7I5HFf4UC9ETmMLjsQUWN00hYuyPu7N"
+); // Replace with your key
 
 const CheckoutButton = () => {
+  const [newPayemnt, setnewPayment] = useState();
+
   const handleClick = async () => {
-    const res = await fetch('http://localhost:8000/create-checkout-session/', {
-      method: 'POST',
+    const res = await fetch("http://localhost:8000/create-checkout-session/", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        product_name: 'T-shirt',
+        product_name: "T-shirt",
         amount: 880, // in dollars
       }),
     });
 
     const data = await res.json();
-    console.log("Data ",data)
+    console.log("Data ", data);
     if (data.sessionId) {
       const stripe = await stripePromise;
       await stripe.redirectToCheckout({ sessionId: data.sessionId });
     } else {
-      alert('Failed to create Stripe session.');
+      alert("Failed to create Stripe session.");
     }
   };
+
+  const get_payment = async () => {
+    const response = await get_userPayments();
+    console.log("Amount", response);
+    setnewPayment(response);
+  };
+  useEffect(() => {
+    get_payment();
+  }, []);
 
   return <button onClick={handleClick}>Pay Now</button>;
 };
