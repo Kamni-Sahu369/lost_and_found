c5a00a8b28745913bde14318da3372688345116d
Kamini Sahu <sahunami843525@gmail.com>
2026-04-29 01:58:03 +0530

    fix: configure Django for Render deployment

--- payment/mypro/settings.py
+++ payment/mypro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url  # ← Yeh add karo
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3xvfxz&z**c!ikz+uy*)a*0z8wwk@1_2%l$0@%@1)m3(jh08=v'
+# SECRET_KEY = 'django-insecure-3xvfxz&z**c!ikz+uy*)a*0z8wwk@1_2%l$0@%@1)m3(jh08=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3xvfxz&z**c!ikz+uy*)a*0z8wwk@1_2%l$0@%@1)m3(jh08=v')
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← SecurityMiddleware ke baad
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,10 +86,10 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 # REST_FRAMEWORK = {
@@ -199,10 +204,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Ourmaill2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'rnyx akzc kwps ebqr'  # NOT your Gmail password
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'Ourmaill2002@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
-# razorpay
-# settings.pyy
-RAZORPAY_KEY_ID = 'rzp_test_s79mdLqr3Mp1I3'
-RAZORPAY_KEY_SECRET = 'ZvPWe37koEohjyloJoOZhfK3'
+RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID', '')
+RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET', '')
+
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
